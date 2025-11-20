f0814e560b70bb10739ad5b579274d2921b1067c
Saila. Vishnu <105279832+vishnusaila@users.noreply.github.com>
2025-11-20 13:14:45 +0530

    Update gemini_request.py

--- gemini_request.py
+++ gemini_request.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-API_KEY = "MtdNuztiHz74Qc34xc0VhxAVgRxz0F7t8cPQybeU"
+#API_KEY = "MtdNuztiHz74Qc34xc0VhxAVgRxz0F7t8cPQybeU"
 
 # ✅ Use v1 (not v1beta)
 URL = f"https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key={API_KEY}"
@@ -16,3 +16,4 @@ response = requests.post(URL, json=data)
 
 print("Status:", response.status_code)
 print(json.dumps(response.json(), indent=2))
+
